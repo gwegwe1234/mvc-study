8c245c27c05870e4ed0c5e1088b8731f95cf37c1
gwegwe1234 <gwangtae5860@daum.net>
2023-07-04 14:54:51 +0900

    서블릿 내용 추가

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,25 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("[전체 파라미터 조회] - start");
+
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(req.getParameter(paramName)));
+
+        System.out.println("[전체 파라미터 조회] - end");
+
+        System.out.println(req.getParameter("username"));
+    }
+}
